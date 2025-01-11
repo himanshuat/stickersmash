85b295010c381cbcaa9c95d403d555a9d20f5263
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2025-01-11 22:57:35 +0530

    Implement Stack navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.text}>Home screen</Text>
+			<Link style={styles.button} href="/about">Go to About screen</Link>
 		</View>
 	);
 }
@@ -16,6 +18,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 	text: {
+		color: '#fff',
+	},
+	button: {
+		fontSize: 20,
+		textDecorationLine: 'underline',
 		color: '#fff',
 	},
 })
