17bc76600e03fede1ca6dcbe621731d3eff32e77
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2025-01-12 19:03:22 +0530

    Use Gesture Handler to add gestures

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { type ImageSource } from 'expo-image';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -51,7 +52,7 @@ export default function Index() {
 	};
 
 	return (
-		<View style={styles.container}>
+		<GestureHandlerRootView style={styles.container}>
 			<View style={styles.imageContainer}>
 				<ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
 				{pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -73,7 +74,7 @@ export default function Index() {
 			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
 				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
 			</EmojiPicker>
-		</View>
+		</GestureHandlerRootView>
 	);
 }
 
