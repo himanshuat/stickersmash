6cf09114e616f55d7f652e58de3e36cccfac397d
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2025-01-12 16:18:29 +0530

    Create an emoji picker modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,12 +6,15 @@ import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
 	const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
 	const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
 
 	const pickImageAsync = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,7 +36,11 @@ export default function Index() {
 	};
 
 	const onAddSticker = () => {
-		// To be implemented
+		setIsModalVisible(true);
+	};
+
+	const onModalClose = () => {
+		setIsModalVisible(false);
 	};
 
 	const onSaveImageAsync = async () => {
@@ -59,6 +66,9 @@ export default function Index() {
 					<Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
 				</View>
 			)}
+			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+				{/* A list of emoji component will go here */}
+			</EmojiPicker>
 		</View>
 	);
 }
