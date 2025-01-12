7574511756a652e1dedd6302bc9a44872140bb6b
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2025-01-12 18:09:19 +0530

    Display list of emojis on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { type ImageSource } from 'expo-image';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 import IconButton from '@/components/IconButton';
 import CircleButton from '@/components/CircleButton';
 import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -14,7 +16,7 @@ export default function Index() {
 	const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
 	const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-
+	const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
 	const pickImageAsync = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
@@ -67,7 +69,7 @@ export default function Index() {
 				</View>
 			)}
 			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-				{/* A list of emoji component will go here */}
+				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
 			</EmojiPicker>
 		</View>
 	);
